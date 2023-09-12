bfc4f5a92d9cc16fe426de6d4e6c03747c9e53c3
tamarin-tandra <ttandra3@gatech.edu>
2023-09-11 21:16:53 -0400

    Changed blast program from blastn to tblastn

--- find_homologs.sh
+++ find_homologs.sh
@@ -6,7 +6,7 @@ subject_file="$2"
 output_file="$3"
 
 #Perform BLAST search and filter for perfect matches
-blastn -query "$query_file" -subject "$subject_file" -perc_identity 100 -ungapped -outfmt "6 sseq" -out "$output_file"
+tblastn -query "$query_file" -subject "$subject_file" -perc_identity 100 -ungapped -outfmt "6 sseq" -out "$output_file"
 
 #Print the number of perfect matches to stdout
 perfect_match_count=$(wc -l < "$output_file")
