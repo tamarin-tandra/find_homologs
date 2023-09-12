72318e15d0247447f04cdbbd81a2ac1f76104dcd
tamarin-tandra <ttandra3@gatech.edu>
2023-09-11 21:32:27 -0400

    Updated find_homologs.sh to perform blast search and filter with >30% sequence identity and >90% match length

--- find_homologs.sh
+++ find_homologs.sh
@@ -6,8 +6,9 @@ subject_file="$2"
 output_file="$3"
 
 #Perform BLAST search and filter for perfect matches
-tblastn -query "$query_file" -subject "$subject_file" -perc_identity 100 -ungapped -outfmt "6 sseq" -out "$output_file"
+tblastn -query "$query_file" -subject "$subject_file" -outfmt "6 qseqid sseqid pident length" | \
+    awk '$3 > 30 && $4 > 0.9 * length ($1)' > "$output_file"
 
 #Print the number of perfect matches to stdout
-perfect_match_count=$(wc -l < "$output_file")
-echo "Number of perfect matches: $perfect_match_count"
+number_of_matches=$(wc -l < "$output_file")
+echo "Number of matches: $number_of_matches"
